ad067915de3e990e4fbe09fb84f05b48a22fb5ce
Oroz <oroz.asan97@gmail.com>
2020-04-30 12:13:11 +0300

    fix2
    Добавлены комментарии к js-коду

--- test/main.js
+++ test/main.js
@@ -1,3 +1,4 @@
+//Если хранилище пустое, то обращаемся к json-файлу
 if (localStorage.length<1){
 	var jsondata = new XMLHttpRequest();
 	jsondata.open('GET', 'lpu.json', false);
@@ -5,6 +6,7 @@ if (localStorage.length<1){
 	if (jsondata.status != 200) {
 		alert( jsondata.status + ': ' + jsondata.statusText );
 	} 
+	//Получив ответ в виде данных, записываем их в хранилище
 	else {
 		var data = JSON.parse(jsondata.responseText);
 		for (var i = 0; i<data.length; i++){
@@ -21,12 +23,15 @@ var table = document.getElementById("maintable");
 var tbody = table.getElementsByTagName("TBODY")[0];
 var k = 0;
 for(var i = 0;i<localStorage.length; i++) {
+	//Если существует пробел в последовательности (key1,key2,key5,key6), то следующие данные вставляем в него
 	if (localStorage.getItem("key"+k)==null){
 		while (localStorage.getItem("key"+k)==null){
 			k=k+1;
 		}
 	}
+	//Разбираем строку JSON
 	var item = JSON.parse(localStorage.getItem('key'+k));
+	//Создаем строку в таблице и заполняем первые 3 ячейки данными из хранилища
     var row = document.createElement("TR");
     var td1 = document.createElement("TD");
     td1.appendChild(document.createTextNode(item.full_name));
@@ -35,6 +40,7 @@ for(var i = 0;i<localStorage.length; i++) {
     var td3 = document.createElement("TD");
     td3.appendChild(document.createTextNode(item.phone));
     var td4 = document.createElement("TD");
+    //В остальные ячейки вставляем кнопки редактирования и удаления с соответствующими функциями
     var a1 = document.createElement("a");
     a1.className = "btn btn-secondary";
     a1.innerHTML = "Edit";
@@ -45,6 +51,7 @@ for(var i = 0;i<localStorage.length; i++) {
     a2.className = "btn btn-danger";
     a2.innerHTML = "Delete";
     a2.setAttribute("onclick","del(this,'key"+k+"')");
+    //Добавляем все ячейки к строке и добавляем строку в таблицу
     td5.appendChild (a2);
     row.appendChild(td1);
     row.appendChild(td2);
@@ -55,16 +62,20 @@ for(var i = 0;i<localStorage.length; i++) {
     k++;
 }
 
+//функция для передачи данных строки в поля input
 function updaterow(key,row) {
-	var index = row.parentNode.parentNode.rowIndex;
+	var index = row.parentNode.parentNode.rowIndex;//индекс строки для передачи в функцию редактирования
+	//"меняем" кнопку Добавить на Сохранить
 	document.getElementById("addbutton").hidden = true;
 	document.getElementById("changebutton").hidden = false;
 	var rowdata = JSON.parse(localStorage.getItem(key));
 	document.getElementById("full_name").value = rowdata.full_name;
 	document.getElementById("address").value = rowdata.address;
 	document.getElementById("phone").value = rowdata.phone;
+	//Присваиваем кнопке Сохранить функцию для редактирования данных в хранилище
 	document.getElementById("changebutton").setAttribute('onclick',"updatedata('"+key+"',"+index+")");
 	var theElement = document.getElementById("inputdata");
+	//Скроллинг
     var selectedPosX = 0;
     var selectedPosY = 0;
     while (theElement != null) {
@@ -75,17 +86,21 @@ function updaterow(key,row) {
     window.scrollTo(selectedPosX,selectedPosY);
 }
 
+
+//Функция для редактирования данных в localstorage
 function updatedata(key,row){
 	document.getElementById("full_name").value=document.getElementById("full_name").value.trim();
 	document.getElementById("address").value= document.getElementById("address").value.trim();
 	document.getElementById("phone").value=document.getElementById("phone").value.trim();
 
 	var forms = document.getElementsByClassName('addnote_form');
+	//Валидация
 	var validation = Array.prototype.filter.call(forms, function(form) {
 		if (form.checkValidity() === false) {
 			form.classList.add('was-validated');
 		}
 		else{
+			//Обновляем данные по ключу и обновляем значения в ячейках
 			var kek = JSON.parse(localStorage.getItem(key));
 			var obj = {
 				full_name: document.getElementById("full_name").value,
@@ -107,6 +122,7 @@ function updatedata(key,row){
 	});
 }
 
+//Функция удаления записи
 function del(i,key){
 	var index = i.parentNode.parentNode.rowIndex;
 	var table = document.getElementById("maintable").deleteRow(index);
@@ -116,13 +132,14 @@ function del(i,key){
 	document.getElementById("phone").value = "";
 }
 
-
+//функция добавления записи
 function addnote(){
 	document.getElementById("full_name").value=document.getElementById("full_name").value.trim();
 	document.getElementById("address").value= document.getElementById("address").value.trim();
 	document.getElementById("phone").value=document.getElementById("phone").value.trim();
 	
 	var forms = document.getElementsByClassName('addnote_form');
+	//Валидация
 	var validation = Array.prototype.filter.call(forms, function(form) {
 		if (form.checkValidity() === false) {
 			form.classList.add('was-validated');
